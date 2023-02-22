ca77a9a5d4c630ca59a395c85f5eb567fdc74074
Alexandria <alex.pugia816@hotmail.com>
2023-02-22 10:10:39 -0500

    differnet url for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://alex-challenge.web.app/'
+    origins 'https://alex-challenge.web.app'
 
     resource '*',
       headers: :any,
